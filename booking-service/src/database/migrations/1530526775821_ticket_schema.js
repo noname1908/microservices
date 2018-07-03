6ac00d5eea92dd8a31d480f73fee2ea98385e6b1
Duy Nguyen <vanduy19082018@gmail.com>
2018-07-03 17:30:03 +0700

    booking schema

--- booking-service/src/database/migrations/1530526775821_ticket_schema.js
+++ booking-service/src/database/migrations/1530526775821_ticket_schema.js
@@ -6,6 +6,9 @@ class TicketSchema extends Schema {
   up () {
     this.create('tickets', (table) => {
       table.increments()
+      table.integer('schedule_id').unsigned()
+      table.integer('seats')
+      table.integer('totalAmount')
       table.timestamps()
     })
   }
